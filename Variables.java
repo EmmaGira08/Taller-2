b39e683101b26592c7be0ee2b4a25e30ad15d98f
Emmanuel G. <emmanuel.giraldoa@upb.edu.co>
2026-08-17 15:54:26 -0500

    Commit 4 declarar e inicializar variables de tipo entero

--- Variables.java
+++ Variables.java
@@ -1,5 +1,10 @@
 public class Variables {
     public static void main(String[] args) {
         System.out.println("Taller 2: Variables y Condicionales");
+        // 1 a 4: Enteros Primitivos
+byte edadEstudiante = 20;
+short anioActual = 2026;
+int cantidadUsuarios = 1500;
+long poblacionMundial = 8000000000L; // Uso del sufijo 'L' (Punto 8)
     }
 }
