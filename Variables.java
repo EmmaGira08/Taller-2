6f850f8385d22daa021bdb79d5b0f33e251d2963
Emmanuel G. <emmanuel.giraldoa@upb.edu.co>
2026-08-17 15:57:24 -0500

    Commit 5 declarar e inicializar variables flotantes y double con sufijo f

--- Variables.java
+++ Variables.java
@@ -6,5 +6,10 @@ byte edadEstudiante = 20;
 short anioActual = 2026;
 int cantidadUsuarios = 1500;
 long poblacionMundial = 8000000000L; // Uso del sufijo 'L' (Punto 8)
+// 5 a 8: Flotantes y Decimales
+float temperaturaCelsius = 23.5f; // Uso del sufijo 'f' (Punto 8)
+float velocidadMantenida = 80.0f;
+double precioDolar = 3950.50;
+double notaPromedio = 4.2;
     }
 }
