ccdba5542a0128b323f08dc199d8640be5996e2c
juan pablo <juan.moralesmel@upb.edu.co>
2026-08-17 20:22:05 -0500

    commit 15 calcular el numerador principal

--- ecuaciones.java
+++ ecuaciones.java
@@ -24,6 +24,13 @@ double x = scanner.nextDouble();
 System.out.print("Ingrese el valor de z: ");
 double z = scanner.nextDouble();
 
+// Numerador: (3x) / (1 + (3x) / (3z^2 + 2))
+double num1 = 3 * x;
+double denSub1 = 3 * Math.pow(z, 2) + 2;
+double denNum = 1 + (num1 / denSub1);
+double numeradorPrincipal = num1 / denNum;
+
+
         
         break;
 
