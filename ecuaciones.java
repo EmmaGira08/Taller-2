b979fbec978e29f694287f99eab60682736fd836
juan pablo <juan.moralesmel@upb.edu.co>
2026-08-17 20:16:03 -0500

    commit 12 creación menu

--- ecuaciones.java
+++ ecuaciones.java
@@ -1,4 +1,15 @@
 import java.util.Scanner;
-
-// Dentro del método main:
+public class ecuaciones {
+    public static void main(String[] args) {
+    // Dentro del método main:
 Scanner scanner = new Scanner(System.in);
+
+
+System.out.println("\n=== MENÚ DE OPCIONES ===");
+System.out.println("1. Solución ecuación 1");
+System.out.println("2. Solución ecuación 2");
+System.out.println("3. Salir");
+System.out.print("Seleccione una opción: ");
+
+    }
+}
